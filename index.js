d4e739bedcf7caf9f5f144e11478791187e2d3e3
miqueiasvianasilva21 <miqueias@icomp.ufam.edu.br>
2023-10-15 12:48:05 -0400

    corrigindo a conexao

--- index.js
+++ index.js
@@ -29,27 +29,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-async function inserirUsuario(usuario) {
-  const client = new MongoClient(uri);
 
-  try {
-    await client.db("lendasamazonia").command({ ping: 1 });
-    console.log("Conectado ao mongoDB");
-
-      const db = client.db("lendasamazonia");
-      const collection = db.collection("usuario");
-
-      const result = await collection.insertOne(usuario);
-      console.log(`Usuário inserido com sucesso. ID: ${result.insertedId}`);
-  } catch (err) {
-      console.error('Erro ao inserir o usuário:', err);
-  } finally {
-      client.close();
-      console.log('Conexão fechada');
-  }
-}
-
-// Rota para adicionar um usuário
 app.post("/adduser", async (req, res) => {
   try {
     const { nome, idade } = req.body;
@@ -63,16 +43,7 @@ app.post("/adduser", async (req, res) => {
     res.status(500).json({ error: "Erro ao adicionar o usuário", details: error.message });
   }
 });
-app.post("/register", (req, res) => {
-  const usuario = {
-      nome :req.body.nome,
-      idade:req.body.idade
-  };
 
-  inserirUsuario(usuario);
-
-  res.send({ msg: "Usuário cadastrado com sucesso" });
-});
 
 app.get("/",(req,res)=>{
   res.send("Hello");
